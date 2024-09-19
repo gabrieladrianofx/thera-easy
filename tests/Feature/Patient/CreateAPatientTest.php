83d7f1d9b904c5ae09edacf29915989b2604ebbf
gabrieladrianofx <gabrieladrianofx@gmail.com>
2024-09-19 16:01:32 -0300

    thera-14: it should allow patient registration when the contact field is filled in and regex is validated

--- tests/Feature/Patient/CreateAPatientTest.php
+++ tests/Feature/Patient/CreateAPatientTest.php
@@ -109,3 +109,22 @@ it('should allow registering a patient when the marital status field is informed
     ]);
     assertDatabaseCount('patients', 1);
 });
+
+it('should allow patient registration when the contact field is filled in and regex is validated', function () {
+    $user = User::factory()->create();
+    actingAs($user);
+
+    post(route('patient.store', [
+        'CPF'            => str_repeat('1', 11),
+        'name_patient'   => str_repeat('*', 150),
+        'contact_number' => '84932129632',
+    ]))->assertRedirect();
+
+    post(route('patient.store', [
+        'CPF'            => str_repeat('2', 11),
+        'name_patient'   => str_repeat('*', 150),
+        'contact_number' => '98743256',
+    ]))->assertSessionHasErrors(['contact_number' => 'The contact_number field must be a valid phone number in the format (XX) XXXX-XXXX, (XX) XXXXX-XXXX, or XXXXXXX-XXXX.']);
+
+    assertDatabaseHas('patients', ['contact_number' => '84932129632']);
+});
